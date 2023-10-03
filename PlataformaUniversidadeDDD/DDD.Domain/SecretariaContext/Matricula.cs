a25307dca038513efb853ff99d71c6a19d6bbe5a
ti.estagiario <ti.estagiario@TI-12639>
2023-10-03 09:32:51 -0300

    commitInitial

---
+++ PlataformaUniversidadeDDD/DDD.Domain/SecretariaContext/Matricula.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.SecretariaContext
+{
+    public class Matricula
+    {
+        public int MatriculaId { get; set; }
+
+        public Aluno Aluno { get; set; }
+
+        public Disciplina Disciplina { get; set; }
+
+        public DateTime Data { get; set; }
+
+    }
+}
