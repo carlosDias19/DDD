c7ec63a71d5111b44ed184e650b729c52cb67a4d
Carlos_Dias <carloseduardotupa19@gmail.com>
2023-10-26 22:40:06 -0300

    commitTeste

---
+++ PlataformaUniversidadeDDD/DDD.Domain/ReportRadarContext/Estado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.ReportRadarContext
+{
+    public class Estado
+    {
+        [Key]
+        public int EstadoId { get; set; }
+        public string NomeEstado { get; set; }
+
+    }
+}
